1ec5091da6e7c90a1ff328bf8632e5fbb7fe7c48
Martinusbe <martinusbe@gmail.com>
2015-12-06 07:41:39 +0100

    bc: adjust to John's changes
    Change-Id: I25baec1c6e11cfee0f427b4a6c015138eba009ce
    Signed-off-by: Martinusbe <martinusbe@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -51,10 +51,10 @@ GRAPHITE_OPTS := false
 ENABLE_GCCONLY := true
 
 # Kernel Toolchain
-KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-5.2-uber/bin
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-5.2/bin
 KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
 # Rom Toolchain
-TARGET_GCC_VERSION_EXP := 5.2-uber
+TARGET_GCC_VERSION_EXP := 5.2
 
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
