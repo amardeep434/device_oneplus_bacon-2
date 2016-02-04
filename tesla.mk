e3cbbb424476a4b7729055f875844377df71be26
Martinusbe <martinusbe@gmail.com>
2016-02-04 09:23:42 +0100

    bc: tc uber 4.9 for battery
    Change-Id: I3f2554c67c3bd4fe86151e990a4df4b544773023
    Signed-off-by: Martinusbe <martinusbe@gmail.com>

--- tesla.mk
+++ tesla.mk
@@ -37,6 +37,10 @@ TARGET_VENDOR_PRODUCT_NAME := bacon
 TARGET_VENDOR_DEVICE_NAME := A0001
 PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=A0001 PRODUCT_NAME=bacon
 
+# My additions
+PRODUCT_PACKAGES += \
+    Snap
+
 ## Use the latest approved GMS identifiers unless running a signed build
 ifneq ($(SIGN_BUILD),true)
 PRODUCT_BUILD_PROP_OVERRIDES += \
