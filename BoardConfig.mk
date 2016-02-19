e3b9a95103126864d826f57a37af116e6ca47aab
Martinusbe <martinusbe@gmail.com>
2016-02-19 17:21:17 +0100

    bc: fixus derpus
    Change-Id: I488f240145b5a15b74def6213a097fb946775f8c
    Signed-off-by: Martinusbe <martinusbe@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -52,7 +52,7 @@ ENABLE_GCCONLY := true
 
 # Kernel Toolchain
 KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.9-uber/bin
-#KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
+KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
 # Rom Toolchain
 TARGET_GCC_VERSION_EXP := 4.9-uber
 
